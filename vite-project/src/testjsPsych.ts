dfe533895653fa2663014374737639b9de43d4c4
david-roper <davidroper321@gmail.com>
2024-07-29 14:29:35 -0400

    add more content to jspsych trial

--- vite-project/src/testjsPsych.ts
+++ vite-project/src/testjsPsych.ts
@@ -2,3 +2,28 @@ import {initJsPsych} from 'jspsych';
 
 
 const jsPsych = initJsPsych();
+import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
+const trial = {
+    type: htmlKeyboardResponse,
+    stimulus: `
+    <p>In this experiment, a circle will appear in the center 
+    of the screen.</p><p>If the circle is <strong>blue</strong>, 
+    press the letter F on the keyboard as fast as you can.</p>
+    <p>If the circle is <strong>orange</strong>, press the letter J 
+    as fast as you can.</p>
+    <div style='width: 700px;'>
+    <div style='float: left;'><img src='img/blue.png'></img>
+    <p class='small'><strong>Press the F key</strong></p></div>
+    <div style='float: right;'><img src='img/orange.png'></img>
+    <p class='small'><strong>Press the J key</strong></p></div>
+    </div>
+    <p>Press any key to begin.</p>
+  `,
+  post_trial_gap: 2000
+}
+
+
+export default testTask => {
+
+   return jsPsych.run([trial]);
+}
